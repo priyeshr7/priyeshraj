9f5cb9eb318f6bf7e4bd687aee9bbb8dd8889b09
Priyesh <46111370+priyeshr7@users.noreply.github.com>
2025-04-17 09:07:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={basePath}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
